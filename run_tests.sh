0286683ea9b3a90aaa71002eb25bbad87fe5b51d
rpedde <ron@pedde.com>
2014-09-09 05:26:47 -0500

    add pyflakes to test

--- run_tests.sh
+++ run_tests.sh
@@ -7,6 +7,8 @@ case "${WHAT}" in
         nosetests -v $(dirname $0)/tests/*test*.py
         ;;
     pep8)
-        pep8 --repeat --show-pep8 --show-source $(dirname $0)/userapi
+#        pep8 --repeat --show-pep8 --show-source $(dirname $0)/userapi
+        pep8 $(dirname $0)/userapi
+        pyflakes $(dirname $0)/userapi
         ;;
 esac
